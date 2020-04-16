c9746bb871c19901f8d4c21a317fee42a01d5c0b
Airy <a532710813@gmail.com>
2020-04-16 14:47:22 +0800

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:planets_card/ui/home/home_page.dart';
+
+void main() => runApp(
+  new MaterialApp(
+    title: "Airy Planets",
+    home: new HomePage(),
+  )
+);
